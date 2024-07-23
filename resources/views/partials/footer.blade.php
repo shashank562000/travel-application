bb33c019371d52cbb0ed9cc3f6f6fa961936a810
Abhishek <absk1901mff@gmail.com>
2024-07-23 22:38:22 +0530

    css files with blade

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -49,19 +49,6 @@
                 <i class="fa fa-facebook-official" ></i>
             </div>
         </div>
-        <div class="row">
-            <div class="col-4 mb-3">
-                <small class="text-white"> &copy; 2010-2024 - ALL RIGHTS RESERVED </small>
-            </div>
-            <div class="col-4 text-center align-items-center justify-content-center" style="justify-content:center;display:flex;">
-                <img src="../resources/images/mastercard.png" style="height:30px;width:50px">
-                <img src="../resources/images/visa.png" style="height:30px;width:50px">
-            </div>
-            <div class="col-4">
-                <small class="text-white"> TERMS & CONDITIONS </small>
-                <small class="text-white mx-2"> PRIVACY POLICY </small>
-                <small class="text-white mx-2"> SUPPORT CENTER </small>
-            </div>
-        </div>
+        @include('partials.footer-bottom')
     </div>
     </section>

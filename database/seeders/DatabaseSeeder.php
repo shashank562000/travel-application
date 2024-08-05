269f18ac351f1a0ac6952a6b4696864bfcd8e13f
Abhishek <absk1901mff@gmail.com>
2024-08-05 23:26:36 +0530

    one seeder command

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Database\Seeders\PageSeeder;
+use Database\Seeders\PageSettings;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,11 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            PageSeeder::class,
+            PageSettings::class
         ]);
     }
 }
